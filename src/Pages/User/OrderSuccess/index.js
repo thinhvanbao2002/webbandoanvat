c345e8f9a22045a4b3c787a6fb0e9e41061bf539
Thinh Van Bao <thinhbao817@gmail.com>
2023-12-22 10:57:00 +0700

    update

--- src/Pages/User/OrderSuccess/index.js
+++ src/Pages/User/OrderSuccess/index.js
@@ -4,7 +4,17 @@ function OrderSuccess(props) {
     return (
         <>
             <div className="wrapper-order-success">
-
+                <div className="order-success-body">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="16  0" width="160"
+                         viewBox="0 0 448 512">
+                        <path
+                            d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"
+                            fill="green"
+                        />
+                    </svg>
+                    <p href=""> <span>Đặt hàng thành công!</span></p>
+                    <a href="">Tiếp tục đặt hàng.</a>
+                </div>
             </div>
         </>
     );
